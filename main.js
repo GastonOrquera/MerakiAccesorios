9bf0b82253a8298d15383309992baa7eb0c0cdd3
GastonOrquera <gastonorquera29@gmail.com>
2024-11-02 02:48:44 -0300

    MerakiPage

---
+++ main.js
@@ -0,0 +1,12 @@
+let nav = document.querySelector('nav');
+let a_links = document.querySelectorAll('.a_links');
+let hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', () => {
+
+    nav.classList.toggle('active');
+
+
+
+
+});
